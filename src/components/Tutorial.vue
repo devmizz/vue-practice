73d1ac49e3df59a80de3c43165fbc38b6cbd3866
devmizz <garamminchan@gmail.com>
2024-11-03 23:27:57 +0900

    Form Bindings

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -4,17 +4,23 @@ import {reactive, ref} from 'vue'
 const counter = reactive({
   count: 0
 })
+const message = ref('Hello World!')
+const titleClass = ref('title')
+const text = ref('')
+const textClass = ref('text')
+
 console.log(counter.count)
 counter.count++
 
-const message = ref('Hello World!')
 console.log(message.value)
 message.value = 'Changed'
 
-const titleClass = ref('title')
-
 function increment() {
   counter.count++
+}
+
+function onInput(e) {
+  text.value = e.target.value
 }
 </script>
 
@@ -23,12 +29,22 @@ function increment() {
   <p>Count is {{ counter.count }}</p>
   <h1 v-bind:class="titleClass">Make me dynamic!</h1>
   <h1 :class="titleClass">{{ message.split('').reverse().join('') }}</h1>
-  <button v-on:click="increment">Count is {{ counter.count }}</button>
-  <button @click="increment">Count is {{ counter.count }}</button>
+  <div>
+    <button v-on:click="increment">Count is {{ counter.count }}</button>
+    <button @click="increment">Count is {{ counter.count }}</button>
+  </div>
+  <div>
+    <input :value="text" @input="onInput" placeholder="Type here">
+    <input :value="text" v-on:input="onInput" placeholder="Type here">
+    <p :class="textClass">{{text}}</p>
+  </div>
 </template>
 
 <style>
 .title {
   color: red;
+}
+.text {
+  color: white;
 }
 </style>
